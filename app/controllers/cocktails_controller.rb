5982fb6e644e66656d75ecb2e2984889345821f8
Faustine Fourdinier <lafaustinbox@gmail.com>
2019-08-15 16:43:46 +0200

    back-end done

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,31 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+    console
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    console
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    # sucre synthaxique
+    # redirect_to cocktail_path(id: @cocktail.id)
+    # redirect_to cocktail_path(@cocktail)
+    redirect_to @cocktail
+
+  end
+
+  private
+  # To whitelist the fields your users can act on with strong params:
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+
+# on parse des vrais noms d'ingredients sur la Json list: https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list
+require 'json'
+require 'open-uri'
+
+puts "creating seeds"
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+list = JSON.parse(user_serialized)
+
+20.times do
+Ingredient.create(name: list['drinks'][rand(0..159)]['strIngredient1'])
+end
+puts "finished"
