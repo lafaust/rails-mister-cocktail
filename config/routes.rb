48493eb4f512603775994c81a251ec3f2e79214f
Faustine Fourdinier <lafaustinbox@gmail.com>
2019-08-16 11:46:47 +0200

    redirect root to

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     # resources :ingredients, only: [:index, :show]
+    root to: redirect('/cocktails')
+
     resources :cocktails, only: [:new, :create, :index, :show] do
       resources :doses, only: [:new, :create]
     end
